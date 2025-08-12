70abc7c839ad3c2651cbd0134ea673ef4ea326ac
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2025-08-12 14:30:53 +0500

    Initial

---
+++ main.js
@@ -0,0 +1,15 @@
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+let main = document.querySelector(".main-section");
+let footer = document.querySelector(".footer");
+let darkMode = document.querySelector(".header-btn");
+
+darkMode.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    header.classList.toggle("dark-mode");
+    main.classList.toggle("dark-mode2");
+    footer.classList.toggle("dark-mode2");
+})
+
+let search = document.querySelector(".main-input");
+let inputValue = search.value;
